5a3ec3d2ade62af75d74823d6c2c73fbfb1ff9c8
arefinMilen <arefin15-5279@diu.edu.bd>
2026-01-26 23:57:35 +0600

    complete notes management with using useState

--- state-management/src/App.jsx
+++ state-management/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import States from './components/States'
+import Notes from './components/Notes.jsx'
 
 const App = () => {
   return (
     <div>
       <States />
+      <Notes  />
     </div>
   )
 }
