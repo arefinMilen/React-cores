c732d95f473170e844f86a06cedb8c7d2ad705a2
arefinMilen <arefin15-5279@diu.edu.bd>
2026-02-04 12:40:09 +0600

    fetch data from api and create image gallery from picsum apis

---
+++ Api-calling/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CallingApi from './components/CallingApi'
+import ImageCard from './components/ImageCard'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Props Passing Project</h1>
+      <CallingApi />
+      <ImageCard />
+    </div>
+  )
+}
+
+export default App
